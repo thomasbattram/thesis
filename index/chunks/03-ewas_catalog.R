ef0c7645c819da32146e5ce8d2f2c7a7524e0316
Thomas Battram <tb13101@bristol.ac.uk>
2020-03-21 17:43:31 +0000

    updated chapter 3 to include a figure and made sure it was aligned

---
+++ index/chunks/03-ewas_catalog.R
@@ -0,0 +1,15 @@
+## ---- load_data --------------------------------
+
+fig_dir <- "/Users/tb13101/Desktop/projects/Main_project/thesis/index/figure/03-ewas_catalog/"
+
+catalog_cap <- "Using the EWAS catalog. 
+At the top of the figures is the home page URL, ewascatalog.org. 
+Below that are examples of three types of searches possible: 
+1. CpG sites, 2. genes and 3. traits. 
+Finally, the results are displayed after searching the catalog for “Depression”. 
+Circled in red is the download button, this button enables the user to download the results of their search as a tab-separated value file. 
+This file will contain the information shown on the website as well as additional analysis information."
+
+
+## ---- catalog-use --------------------------------
+include_graphics(paste0(fig_dir, "using_the_catalog.pdf"))
