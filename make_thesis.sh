46cd548493ab217959d41413f1d34d7002ba0f83
Thomas Battram <tb13101@bristol.ac.uk>
2020-06-16 12:30:35 +0100

    outputs .md file so can check for mistakes in development

--- make_thesis.sh
+++ make_thesis.sh
@@ -1,3 +1,3 @@
 cd index/
 
-Rscript -e "require(knitr); require(markdown); require(thesisdown); bookdown::render_book('index.Rmd', output_format = 'all')"
+Rscript -e "require(knitr); require(markdown); require(thesisdown); bookdown::render_book('index.Rmd', output_format = 'all', clean = FALSE)"
