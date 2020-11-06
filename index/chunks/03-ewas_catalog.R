f7d79ae7affc4451751c2ace754ec8a09b1697b0
Thomas Battram <tb13101@bristol.ac.uk>
2020-11-06 21:35:11 +0000

    updated chapter 3 based on nics comments and after re-read

--- index/chunks/03-ewas_catalog.R
+++ index/chunks/03-ewas_catalog.R
@@ -1,14 +1,24 @@
 ## ---- load-data-03 --------------------------------
 
-studies <- read_tsv(file.path(tab_path, "03-ewas_catalog", "clean_studies.txt"))
-results <- read_tsv(file.path(tab_path, "03-ewas_catalog", "clean_results.txt"))
+studies <- read_tsv(file.path(tab_path, "03-ewas_catalog", "studies.txt"))
+results <- read_tsv(file.path(tab_path, "03-ewas_catalog", "results.txt"))
 
 ## ---- catalog-data-setup --------------------------------
 
-n_pubs <- length(unique(studies$PMID)[!is.na(unique(studies$PMID))])
+pub_studies <- studies %>%
+	dplyr::filter(!is.na(PMID), Author != "Battram T")
+
+pub_results <- results %>%
+	dplyr::filter(StudyID %in% pub_studies$StudyID) 
+
+n_pubs <- length(unique(pub_studies$PMID))
 n_aries <- sum(studies$Consortium %in% "ARIES")
 n_geo <- sum(studies$Consortium %in% "GEO")
 n_geo_aries <- sum(n_aries, n_geo)
+
+# geo info
+geo_date <- "2020-10-12"
+geo_exp_n <- 136
 
 ## ---- catalog-use --------------------------------
 include_graphics(file.path(fig_path, "03-ewas_catalog", "using_the_catalog.pdf"))
--- meetings/2020-11-06/notes.md
+++ meetings/2020-11-06/notes.md
@@ -17,6 +17,11 @@ Attendees: Tom B, Gib
 	+ etc.
 
 * Inconsistencies between chapters/justifying decisisons made by others (or made 3 years ago)
+	+ Thresholds needed -- explain why they are needed and what the thresholds chosen were arbitrary, but with reasons:
+		+ P < 1e-4 -- less stringent than conventional threshold as wanted to be able to see replication across studies, but not too lenient as that would be computationally difficult to handle
+		+ N > 100 -- wanted to be low enough to include studies of rarer phenotypes and in less studied samples (e.g. non-Europeans), but didn't want to include studies that were very underpowered
+		+ ncpg > 100,000
+	+ Need broad reasons for arbritrary choices -- have a reason for each one! 
 
 __NOTES:__
 
